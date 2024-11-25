c68a2d8279d8c9f6675ab371737b2cdbe92a8a7b
Koji NAKAMURA <kozy4324@gmail.com>
2024-11-25 17:05:42 +0900

    first commit

---
+++ lib/ruby/lsp/rake/go_to_task_def/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Ruby
+  module Lsp
+    module Rake
+      module GoToTaskDef
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "ruby/lsp/rake/go_to_task_def"
+
+require "minitest/autorun"
